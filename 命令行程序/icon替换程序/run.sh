13db53b49f7f4f49c84fa1a7939116e6c5f275d1
Baird-weng <644672334@qq.com>
2017-09-05 10:07:25 +0800

    no message

--- 命令行程序/icon替换程序/run.sh
+++ 命令行程序/icon替换程序/run.sh
@@ -1,38 +1,31 @@
 
 #!/ bin / bash
 #进入当前文件夹，工作区域。
-result=''
-spliter(){
- OLD_IFS="$IFS" 
- IFS="," 
- arr=($1) 
- IFS="$OLD_IFS" 
- result="${arr[1]}"
- echo "$result"
- return "$result"
- # return "${arr[$2]}"
-}
-a="one,two,three,four"
-spliter $a 1
-echo "=============$?"
-# eval 'cd `dirname $0`'
-# #获取目录下所有文件
-# flie=$(ls new)
-# #进入文件夹
-# eval 'cd new'
-
-# # testimage='LaunchImage.png'
-# # eval 'file $testimage'
-# index=0;
-# for i in $flie
-# do
-# info=$(file $i)
-# echo $info
-# # eval 'file $i'
-# # # 自增index
-# # index=`expr $index + 1`
-# # #新文件命名
-# # newfile=$newfileName$index".txt"
-# # eval 'mv $i $newfile'
-# # echo '更改'$i'-------------->'$newfile
-# done
+echo "$result"
+eval 'cd `dirname $0`'
+#获取目录下所有文件
+flie=$(ls new)
+#进入文件夹
+eval 'cd new'
+index=0;
+for i in $flie
+do
+info=$(file $i)
+IN=$info;
+IFS=','; arrIN=($IN); unset IFS;
+k=0
+for j in "${arrIN[@]}"; do
+	if [ $k == '1' ]; then
+      echo "2133123213123==$j"	
+    fi		
+    k=`expr $k + 1`
+done
+# echo "====$info"
+# eval 'file $i'
+# 自增index
+index=`expr $index + 1`
+#新文件命名
+# newfile=$newfileName$index".txt"
+# eval 'mv $i $newfile'
+# echo '更改'$i'-------------->'$newfile
+done
